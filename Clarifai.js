b0c62144c6583500b19646b3a1c8771edb6ddaf9
echen122 <36450576+echen122@users.noreply.github.com>
2018-10-20 11:37:36 -0400

    Add files via upload
    Gender recognition

--- Clarifai.js
+++ Clarifai.js
@@ -6,19 +6,18 @@ const app = new Clarifai.App({
 });
 
 // predict the contents of an image by passing in a url
-app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", "http://img.timeinc.net/time/photoessays/2008/people_who_mattered/obama_main_1216.jpg").then(
+app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", "https://samples.clarifai.com/demographics.jpg").then(
   function(response) {
-    //return gender outputs
-    x = response.outputs[0].data.regions[0].data.face.gender_appearance.concepts;
-    console.log(JSON.stringify(x));
-    console.log(JSON.stringify(x[0]));
-    if(x[0].value > x[1].value)
-    {
-         console.log("Male");
-    }
-    else
-    {
-         console.log("Female");
+    for (i in response.outputs[0].data.regions) {
+        x = response.outputs[0].data.regions[i].data.face.gender_appearance.concepts;
+        if(x[0].value > x[1].value)
+    	{
+         	console.log("Male");
+    	}
+    	else
+    	{
+         	console.log("Female");
+    	}
     }
   },
   function(err) {
