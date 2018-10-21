ca765d89ef483c07b9e9fdd12fcade27344f65fc
echen122 <36450576+echen122@users.noreply.github.com>
2018-10-21 01:43:19 -0400

    Add files via upload

--- Clarifai.js
+++ Clarifai.js
@@ -8,7 +8,7 @@ const app = new Clarifai.App({
 });
 
 
-var urls = ["http://static5.uk.businessinsider.com/image/58c29a46e21a9a28008b47b4-1190-625/the-9-youngest-self-made-female-billionaires-in-the-world.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Dwight_Howard_30483967610.jpg/1200px-Dwight_Howard_30483967610.jpg", "http://soappotions.com/wp-content/uploads/2017/10/orange.jpg"];
+var urls = ["http://static5.uk.businessinsider.com/image/58c29a46e21a9a28008b47b4-1190-625/the-9-youngest-self-made-female-billionaires-in-the-world.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Dwight_Howard_30483967610.jpg/1200px-Dwight_Howard_30483967610.jpg", "https://samples.clarifai.com/demographics.jpg", "http://soappotions.com/wp-content/uploads/2017/10/orange.jpg", "https://amp.businessinsider.com/images/58c30d70c14077f2158b45a0-750-563.jpg","http://static5.uk.businessinsider.com/image/58c29a46e21a9a28008b47b4-1190-625/the-9-youngest-self-made-female-billionaires-in-the-world.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Dwight_Howard_30483967610.jpg/1200px-Dwight_Howard_30483967610.jpg", "https://samples.clarifai.com/demographics.jpg", "http://soappotions.com/wp-content/uploads/2017/10/orange.jpg", "https://amp.businessinsider.com/images/58c30d70c14077f2158b45a0-750-563.jpg", "https://samples.clarifai.com/demographics.jpg"];
 var url = "https://samples.clarifai.com/demographics.jpg";
 
 
@@ -17,6 +17,21 @@ async function gender(urls)
     var gender = [];
     var male = 0;
     var female = 0;
+    var white = 0;
+    var black = 0;
+    var hispanic = 0;
+    var asian = 0;
+    var PI = 0;
+    var ME = 0;
+    var AI = 0;
+    var race = [];
+    var age = [];
+    var U21 = 0;
+    var U34 = 0;
+    var U44 = 0;
+    var U54 = 0;
+    var U64 = 0;
+    var old = 0;
     for(var w = 0; w < urls.length;  w ++)
     {
         try {
@@ -35,41 +50,6 @@ async function gender(urls)
                   gender.push["Female"];
                   female += 1;
               }
-          }
-        } catch(e) {
-           console.log('ERROR', e);  // TypeError: failed to fetch
-        }
-      }
-      if(male != 0 && female != 0)
-      {
-        var malePercent = (male * 100) / (male + female + w - w);
-        console.log("Male: " + malePercent + "%");
-        var femalePercent = (female * 100) / (male + female);
-        console.log("Female: " + femalePercent + "%");
-        var diff = Math.abs(malePercent - femalePercent);
-        var genderScore = (1 - (diff / 100)) * 100;
-        console.log("Gender Score: " + genderScore);
-      }
-}
-
-
-
-async function race(urls)
-{
-    var white = 0;
-    var black = 0;
-    var hispanic = 0;
-    var asian = 0;
-    var PI = 0;
-    var ME = 0;
-    var AI = 0;
-    var race = [];
-    for(var w = 0; w < urls.length;  w ++)
-    {
-        try {
-          const response = await app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", urls[w]);
-          for (i in response.outputs[0].data.regions)
-          {
               y = response.outputs[0].data.regions[i].data.face.multicultural_appearance.concepts[0].name;
               race.push(y);
               if(y === "white")
@@ -100,52 +80,6 @@ async function race(urls)
               {
                   asian += 1;
               }
-          }
-        } catch(e) {
-           console.log('ERROR', e); // TypeError: failed to fetch
-        }
-
-    }
-
-    var whitePercent = (white * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("White: " + whitePercent + "%");
-    var blackPercent = (black * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("Black: " + blackPercent + "%");
-    var asianPercent = (asian * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("Asian: " + asianPercent + "%");
-    var hispanicPercent = (hispanic * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("Hispanic: " + hispanicPercent + "%");
-    var AIPercent = (AI * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("American Indian: " + AIPercent + "%");
-    var PIPercent = (PI * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("Pacific Islander: " + PIPercent + "%");
-    var MEPercent =  (ME * 100) / (white + asian + black + hispanic + PI + AI + ME);
-    console.log("Middle Eastern: " + MEPercent + "%");
-    var std = math.std(whitePercent, blackPercent, asianPercent, hispanicPercent, AIPercent, PIPercent, MEPercent);
-    var raceScore = ((37.8 - std) / 34.3604) * 100;
-    if(raceScore > 100)
-    {
-      raceScore = 100;
-    }
-    raceScore = math.floor((raceScore * 100) / 100);
-    console.log("Race Score: " + raceScore);
-}
-
-async function age(urls)
-{
-    var age = [];
-    var U21 = 0;
-    var U34 = 0;
-    var U44 = 0;
-    var U54 = 0;
-    var U64 = 0;
-    var old = 0;
-    for(var w in urls)
-    {
-        try {
-          const response = await app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", urls[w]);
-          for (i in response.outputs[0].data.regions)
-          {
               var z = response.outputs[0].data.regions[i].data.face.age_appearance.concepts[0].name;
         	    age.push(z);
         	    var a = Number(z);
@@ -175,32 +109,82 @@ async function age(urls)
         	    }
           }
         } catch(e) {
-           console.log('ERROR', e); // TypeError: failed to fetch
+           console.log('ERROR', e);  // TypeError: failed to fetch
         }
-
-    }
-    var U21p = (U21 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    var U34p = (U34 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    var U44p = (U44 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    var U54p = (U54 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    var U64p = (U64 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    var oldp = (old * 100) / (U21 + U34 + U44 + U54 + U64 + old);
-    console.log("21 and Under: " + U21p + "%");
-    console.log("22 - 34: " + U34p + "%");
-    console.log("35 - 44: " + U44p + "%");
-    console.log("45 - 54: " + U54p + "%");
-    console.log("55 - 64: " + U64p + "%");
-    console.log("65+ : " + oldp + "%");
-    std = math.std(U21p, U34p, U44p, U54p, U64p, oldp);
-    var ageScore = ((40.9 - std) / 34.3604) * 100;
-    if(ageScore > 100)
-    {
-        ageScore = 100;
-    }
-    ageScore = math.floor((ageScore * 100) / 100);
-    console.log("Age Score: " + ageScore);
+      }
+      if(male != 0 && female != 0)
+      {
+        var malePercent = (male * 100) / (male + female + w - w);
+        malePercent = math.floor((malePercent * 100) / 100);
+        console.log("Male: " + malePercent + "%");
+        var femalePercent = (female * 100) / (male + female);
+        femalePercent = math.floor((femalePercent * 100) / 100);
+        console.log("Female: " + femalePercent + "%");
+        var diff = Math.abs(malePercent - femalePercent);
+        var genderScore = (1 - (diff / 100)) * 100;
+        console.log("Gender Score: " + genderScore);
+        var whitePercent = (white * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        whitePercent = math.floor((whitePercent * 100) / 100);
+        console.log("White: " + whitePercent + "%");
+        var blackPercent = (black * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        blackPercent = math.floor((blackPercent * 100) / 100);
+        console.log("Black: " + blackPercent + "%");
+        var asianPercent = (asian * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        asianPercent = math.floor((asianPercent * 100) / 100);
+        console.log("Asian: " + asianPercent + "%");
+        var hispanicPercent = (hispanic * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        hispanicPercent = math.floor((hispanicPercent * 100) / 100);
+        console.log("Hispanic: " + hispanicPercent + "%");
+        var AIPercent = (AI * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        AIPercent = math.floor((AIPercent * 100) / 100);
+        console.log("American Indian: " + AIPercent + "%");
+        var PIPercent = (PI * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        PIPercent = math.floor((PIPercent * 100) / 100);
+        console.log("Pacific Islander: " + PIPercent + "%");
+        var MEPercent =  (ME * 100) / (white + asian + black + hispanic + PI + AI + ME);
+        MEPercent = math.floor((MEPercent * 100) / 100);
+        console.log("Middle Eastern: " + MEPercent + "%");
+        var std = math.std(whitePercent, blackPercent, asianPercent, hispanicPercent, AIPercent, PIPercent, MEPercent);
+        var raceScore = ((37.8 - std) / 34.3604) * 100;
+        if(raceScore > 100)
+        {
+          raceScore = 100;
+        }
+        raceScore = math.floor((raceScore * 100) / 100);
+        console.log("Race Score: " + raceScore);
+        var U21p = (U21 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        U21p = math.floor((U21p * 100) / 100);
+        var U34p = (U34 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        U34p = math.floor((U34p * 100) / 100);
+        var U44p = (U44 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        U44p = math.floor((U44p * 100) / 100);
+        var U54p = (U54 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        U54p = math.floor((U54p * 100) / 100);
+        var U64p = (U64 * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        U64p = math.floor((U64p * 100) / 100);
+        var oldp = (old * 100) / (U21 + U34 + U44 + U54 + U64 + old);
+        oldp = math.floor((oldp * 100) / 100);
+        console.log("21 and Under: " + U21p + "%");
+        console.log("22 - 34: " + U34p + "%");
+        console.log("35 - 44: " + U44p + "%");
+        console.log("45 - 54: " + U54p + "%");
+        console.log("55 - 64: " + U64p + "%");
+        console.log("65+ : " + oldp + "%");
+        std = math.std(U21p, U34p, U44p, U54p, U64p, oldp);
+        var ageScore = ((40.9 - std) / 34.3604) * 100;
+        if(ageScore > 100)
+        {
+            ageScore = 100;
+        }
+        ageScore = math.floor((ageScore * 100) / 100);
+        console.log("Age Score: " + ageScore);
+      }
 }
 
+
+
+
+
 gender(urls)
-race(urls)
-age(urls)
+//race(urls)
+//age(urls)
